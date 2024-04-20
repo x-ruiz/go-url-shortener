9ad821010e3d0df5ac4a053086d47534908a920a
Xaviris <the.xavierruiz@gmail.com>
2024-04-19 19:19:57 -0500

    map handler working

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/x-ruiz/urlshort
+
+go 1.22.2
+
+require gopkg.in/yaml.v2 v2.4.0 // indirect
---
+++ handler.go
@@ -0,0 +1,46 @@
+package urlshort
+
+import (
+	"net/http"
+)
+
+// MapHandler will return an http.HandlerFunc (which also
+// implements http.Handler) that will attempt to map any
+// paths (keys in the map) to their corresponding URL (values
+// that each key in the map points to, in string format).
+// If the path is not provided in the map, then the fallback
+// http.Handler will be called instead.
+func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
+	//	TODO: Implement this...
+	fn := func(w http.ResponseWriter, r *http.Request) {
+		path := r.URL.Path
+		url, ok := pathsToUrls[path] // sets url to the value at the key and true/false to ok if exists
+		if ok {
+			http.Redirect(w, r, url, http.StatusSeeOther)
+		} else {
+			fallback.ServeHTTP(w, r)
+		}
+	}
+	return http.HandlerFunc(fn)
+}
+
+// YAMLHandler will parse the provided YAML and then return
+// an http.HandlerFunc (which also implements http.Handler)
+// that will attempt to map any paths to their corresponding
+// URL. If the path is not provided in the YAML, then the
+// fallback http.Handler will be called instead.
+//
+// YAML is expected to be in the format:
+//
+//   - path: /some-path
+//     url: https://www.some-url.com/demo
+//
+// The only errors that can be returned all related to having
+// invalid YAML data.
+//
+// See MapHandler to create a similar http.HandlerFunc via
+// a mapping of paths to urls.
+func YAMLHandler(yml []byte, fallback http.Handler) (http.HandlerFunc, error) {
+	// TODO: Implement this...
+	return nil, nil
+}
